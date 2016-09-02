cbee42c8607d8ed0124c3fc25f30f201c614e80e
Tim Kuijsten <tim@netsend.nl>
2016-09-02 22:00:10 +0200

    new main program

---
+++ main.c
@@ -0,0 +1,23 @@
+#include "jsonify.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXLEN 16 * 1024
+
+int main()
+{
+  char *line = NULL;
+  size_t linesize = 0;
+  ssize_t linelen;
+  char output[MAXLEN];
+
+  while ((linelen = getline(&line, &linesize, stdin)) != -1) {
+    from_loose_to_strict(output, MAXLEN, line, linelen - 1);
+    printf("%s\n", output);
+  }
+  free(line);
+
+  return 0;
+}
