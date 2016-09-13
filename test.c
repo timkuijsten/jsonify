961e52fc9b02a1cdc08236a996f33a92bd3ce96f
Tim Kuijsten <tim@netsend.nl>
2016-09-14 00:43:39 +0200

    rename tests to make room for more tests

--- test.c
+++ test.c
@@ -7,58 +7,58 @@
 
 #define MAXOUTPUT 1024
 
-void run_test(const char *input, const char *expected);
+void test_relaxed_to_strict(const char *input, const char *expected);
 
 int main()
 {
   // one-dimensional
-  run_test("1", "1");
-  run_test("[]", "[]");
-  run_test("[1]", "[1]");
-  run_test("[1,2]", "[1,2]");
+  test_relaxed_to_strict("1", "1");
+  test_relaxed_to_strict("[]", "[]");
+  test_relaxed_to_strict("[1]", "[1]");
+  test_relaxed_to_strict("[1,2]", "[1,2]");
 
   // two-dimensional
-  run_test("[[]]", "[[]]");
-  run_test("[1,[]]", "[1,[]]");
-  run_test("[[],1]", "[[],1]");
-  run_test("[1,[2]]", "[1,[2]]");
-  run_test("[[1],2]", "[[1],2]");
-  run_test("[1,2,[3,4],5]", "[1,2,[3,4],5]");
-  run_test("[1,[2,3,4],5,6]", "[1,[2,3,4],5,6]");
+  test_relaxed_to_strict("[[]]", "[[]]");
+  test_relaxed_to_strict("[1,[]]", "[1,[]]");
+  test_relaxed_to_strict("[[],1]", "[[],1]");
+  test_relaxed_to_strict("[1,[2]]", "[1,[2]]");
+  test_relaxed_to_strict("[[1],2]", "[[1],2]");
+  test_relaxed_to_strict("[1,2,[3,4],5]", "[1,2,[3,4],5]");
+  test_relaxed_to_strict("[1,[2,3,4],5,6]", "[1,[2,3,4],5,6]");
 
   // three-dimensional
-  run_test("[[],1,[2,3],4,[5,[],[6,7],8]]", "[[],1,[2,3],4,[5,[],[6,7],8]]");
+  test_relaxed_to_strict("[[],1,[2,3],4,[5,[],[6,7],8]]", "[[],1,[2,3],4,[5,[],[6,7],8]]");
 
   // four-dimensional
-  run_test("[[[[],[],2],[]],1,[2,3],4,[5,[],[[23,5],[23],[23,73],6,7],8]]", "[[[[],[],2],[]],1,[2,3],4,[5,[],[[23,5],[23],[23,73],6,7],8]]");
+  test_relaxed_to_strict("[[[[],[],2],[]],1,[2,3],4,[5,[],[[23,5],[23],[23,73],6,7],8]]", "[[[[],[],2],[]],1,[2,3],4,[5,[],[[23,5],[23],[23,73],6,7],8]]");
 
   // five-dimensional
-  run_test("[[[[],[],2],[]],1,[2,3],4,[5,[],[[23,5],[23],[23,[23]],6,7],8]]", "[[[[],[],2],[]],1,[2,3],4,[5,[],[[23,5],[23],[23,[23]],6,7],8]]");
+  test_relaxed_to_strict("[[[[],[],2],[]],1,[2,3],4,[5,[],[[23,5],[23],[23,[23]],6,7],8]]", "[[[[],[],2],[]],1,[2,3],4,[5,[],[[23,5],[23],[23,[23]],6,7],8]]");
 
   // six-dimensional
-  run_test("[[],[[[[],[],2],[]],1,[2,3],4,[5,[],[[23,5],[23],[23,[23]],6,7],8]]]", "[[],[[[[],[],2],[]],1,[2,3],4,[5,[],[[23,5],[23],[23,[23]],6,7],8]]]");
+  test_relaxed_to_strict("[[],[[[[],[],2],[]],1,[2,3],4,[5,[],[[23,5],[23],[23,[23]],6,7],8]]]", "[[],[[[[],[],2],[]],1,[2,3],4,[5,[],[[23,5],[23],[23,[23]],6,7],8]]]");
 
   // one-dimensional
-  run_test("{a:true}", "{\"a\":true}");
+  test_relaxed_to_strict("{a:true}", "{\"a\":true}");
 
   // two-dimensional
-  run_test("[{a:true,b:false},[{c:true}]]", "[{\"a\":true,\"b\":false},[{\"c\":true}]]");
+  test_relaxed_to_strict("[{a:true,b:false},[{c:true}]]", "[{\"a\":true,\"b\":false},[{\"c\":true}]]");
 
   // three-dimensional
-  run_test("[{a:true,b:false},[{c:[2,3,{$some:true}]}]]", "[{\"a\":true,\"b\":false},[{\"c\":[2,3,{\"$some\":true}]}]]");
+  test_relaxed_to_strict("[{a:true,b:false},[{c:[2,3,{$some:true}]}]]", "[{\"a\":true,\"b\":false},[{\"c\":[2,3,{\"$some\":true}]}]]");
 
-  run_test(
+  test_relaxed_to_strict(
       "[{$project:{by:true,action:true,on:true,sid:true}},{$match:{action:{$in:['socketstart','socketstop']}}},{$group:{_id:{sid:\"$sid\",uid:\"$by\"},login:{$min:\"$on\"},logout:{$max:\"$on\"}}},{$project:{login:true,logout:true,duration:{$subtract:[\"$logout\",\"$login\"]}}},{$project:{uid:\"$_id.uid\",duration:true,yearWeek:{$dateToString:{format:'%Y%U',date:\"$login\"}}}},{$group:{_id:{uid:\"$uid\",yearWeek:\"$yearWeek\"},duration:{$sum:\"$duration\"}}},{$project:{duration:{$divide:[\"$duration\",60*1000]}}}]",
       "[{\"$project\":{\"by\":true,\"action\":true,\"on\":true,\"sid\":true}},{\"$match\":{\"action\":{\"$in\":[\"socketstart\",\"socketstop\"]}}},{\"$group\":{\"_id\":{\"sid\":\"$sid\",\"uid\":\"$by\"},\"login\":{\"$min\":\"$on\"},\"logout\":{\"$max\":\"$on\"}}},{\"$project\":{\"login\":true,\"logout\":true,\"duration\":{\"$subtract\":[\"$logout\",\"$login\"]}}},{\"$project\":{\"uid\":\"$_id.uid\",\"duration\":true,\"yearWeek\":{\"$dateToString\":{\"format\":\"%Y%U\",\"date\":\"$login\"}}}},{\"$group\":{\"_id\":{\"uid\":\"$uid\",\"yearWeek\":\"$yearWeek\"},\"duration\":{\"$sum\":\"$duration\"}}},{\"$project\":{\"duration\":{\"$divide\":[\"$duration\",60*1000]}}}]");
 
   // multiple docs in one string
-  run_test("{ a: true } { b: \"false\" }  { some: 1234 }", "{\"a\":true}{\"b\":\"false\"}{\"some\":1234}");
+  test_relaxed_to_strict("{ a: true } { b: \"false\" }  { some: 1234 }", "{\"a\":true}{\"b\":\"false\"}{\"some\":1234}");
 
   return 0;
 }
 
 void
-run_test(const char *input, const char *expected)
+test_relaxed_to_strict(const char *input, const char *expected)
 {
   char output[MAXOUTPUT];
 
