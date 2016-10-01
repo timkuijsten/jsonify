ca82373bb4f1fe0a1f3c9a2c8659421c9ef202d1
Tim Kuijsten <tim@netsend.nl>
2016-10-01 18:25:09 +0200

    jsonify: use strlcpy language in api

--- jsonify.h
+++ jsonify.h
@@ -28,6 +28,6 @@
 int pop();
 int push(int val);
 
-int relaxed_to_strict(char *output, size_t outputsize, const char *input, ssize_t inputlen, int firstroot);
+int relaxed_to_strict(char *dst, size_t dstsize, const char *src, ssize_t srclen, int firstonly);
 int iterate(const char *input, jsmntok_t *tokens, int nrtokens, void (*iterator)(jsmntok_t *, char *, int, int, char *));
 void writer(jsmntok_t *tok, char *key, int depth, int ndepth, char *closesym);
